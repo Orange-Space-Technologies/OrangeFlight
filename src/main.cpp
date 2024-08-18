55c15838d34568b191f74f98abb473b0589703f4
HonzaLed <honzaled@proton.me>
2024-08-18 15:41:50 +0200

    Fix indent size in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,28 @@ V_STATE v_state = V_STATE::INIT; // Vehicle state global variable
 StateVector state_vector;
 
 void setup() { //Setup on core0, will initialize hardware and tasks
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // Print the version of the firmware
-  Serial.print("Starting OrangeFlight version ");
-  Serial.print(ORANGEFLIGHT_VERSION);
-  Serial.println("!");
+    // Print the version of the firmware
+    Serial.print("Starting OrangeFlight version ");
+    Serial.print(ORANGEFLIGHT_VERSION);
+    Serial.println("!");
 
-  initGPIO();
-  initSensors();
-  state_vector = StateVector();
-  v_state = V_STATE::IDLE;
+    initGPIO();
+    initSensors();
+    state_vector = StateVector();
+    v_state = V_STATE::IDLE;
 }
 
 void loop() { //Main loop on core0, will run the main control loop
-  
+    
 }
 
 void setup1() { //Setup on core1, will initialize the task system
-  //tasks.push_back(Task(<function pointer>, <name>, <task period in ms>, <priority 0-50, default 10>));
-  //tasks.push_back(Task(*gps_task, "GPS", 1000, 12));
+    //tasks.push_back(Task(<function pointer>, <name>, <task period in ms>, <priority 0-50, default 10>));
+    //tasks.push_back(Task(*gps_task, "GPS", 1000, 12));
 }
 
 void loop1() { //Main loop on core1, will run the task system
-  runMostImportantTask(tasks);
+    runMostImportantTask(tasks);
 }
